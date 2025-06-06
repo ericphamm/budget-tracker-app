fc8dd11bd543e88041f0bcfa93c66e40a10f8f72
ericpham <phamwangthang@gmail.com>
2025-06-06 18:21:41 +0200

    Updated new domain

--- transaction-service/src/main/java/ericphamm/transaction_service/config/CorsConfig.java
+++ transaction-service/src/main/java/ericphamm/transaction_service/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "https://budget-tracker-app-ashen.vercel.app",
+                        "https://budget-tracker-app-ericpham.vercel.app",
                         "http://localhost:5173"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
