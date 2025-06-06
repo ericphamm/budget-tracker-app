abfe729725c54e735f2a22a0787d7f4c76ed6204
ericpham <phamwangthang@gmail.com>
2025-06-06 19:18:18 +0200

    Transaction filter UI fixed

--- transaction-frontend/src/components/TransactionFilter.jsx
+++ transaction-frontend/src/components/TransactionFilter.jsx
@@ -19,17 +19,17 @@ function TransactionFilter({
 			</div>
 
 			{/* search field */}
-			<div className="relative mb-4">
+			<div className="relative">
 				<input
 					type="text"
 					placeholder="Search transactions..."
 					value={searchText}
 					onChange={(e) => setSearchText(e.target.value)}
-					className="w-full px-4 py-2 h-14 rounded-lg border border-gray-300 bg-white text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-sm sm:text-base"
+					className="w-full px-4 py-2 h-14 rounded-lg border border-gray-300 bg-white text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-base"
 					/>
 			</div>
 
-			<div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-2 mb-4 pt-4">
+			<div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-4 pt-4">
 				<div>
 					{/* <label htmlFor="minAmount" className="block text-sm font-medium text-gray-700  mb-1">
 						Min Amount
@@ -39,7 +39,7 @@ function TransactionFilter({
 						placeholder="Min Amount"
 						value={minAmount}
 						onChange={(e) => setMinAmount(e.target.value)}
-						className="w-full px-4 py-2 h-14 rounded-lg border border-gray-300 bg-white text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-sm sm:text-base"
+						className="w-full px-4 py-2 h-14 rounded-lg border border-gray-300 bg-white text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-base"
 						/>
 				</div>
 				
@@ -52,7 +52,7 @@ function TransactionFilter({
 						placeholder="Max Amount"
 						value={maxAmount}
 						onChange={(e) => setMaxAmount(e.target.value)}
-						className="w-full px-4 py-2 h-14 rounded-lg border border-gray-300 bg-white text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-sm sm:text-base"
+						className="w-full px-4 py-2 h-14 rounded-lg border border-gray-300 bg-white text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-base"
 						/>
 				</div>
 			</div>
