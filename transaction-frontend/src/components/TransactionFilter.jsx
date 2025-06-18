8e8bed2f571f0923b15f95213233acde44c3d230
ericpham <phamwangthang@gmail.com>
2025-06-18 13:13:27 +0200

    Show Advanced Filters

--- transaction-frontend/src/components/TransactionFilter.jsx
+++ transaction-frontend/src/components/TransactionFilter.jsx
@@ -1,3 +1,6 @@
+import React, { useState } from "react";
+import { Filter, Search, Calendar } from 'lucide-react';
+
 function TransactionFilter({
 	searchText,
 	setSearchText,
@@ -9,6 +12,8 @@ function TransactionFilter({
 	setMaxAmount,
 	setPage
 }) {
+
+	const [showAdvanced, setShowAdvanced] = useState(false);
 
 	const handleClearFilters = () => {
 		setFilterType('all');      // reset filterType to 'all'
@@ -25,56 +30,18 @@ function TransactionFilter({
 				Filters
 			</h2>
 
-				{(filterType !== 'all' || searchText || minAmount || maxAmount) && (
-					<button
-						onClick={handleClearFilters}
-						className="text-sm text-blue-600 hover:underline"
-					>
-						Clear Filters
-					</button>
-				)}
+				<button
+					onClick={() => setShowAdvanced(!showAdvanced)}
+					className="text-sm text-blue-600 hover:underline flex"
+				>
+					<Calendar size={16} className="mr-1" />
+					{showAdvanced ? "Hide advanced filters" : "Show advanced filters"}
+				</button>
+
 
 			</div>
 
-			{/* search field */}
-			<div className="relative">
-				<input
-					type="text"
-					placeholder="Search transactions..."
-					value={searchText}
-					onChange={(e) => setSearchText(e.target.value)}
-					className="w-full px-4 py-2 h-12 rounded-lg border border-gray-300 bg-white text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-base"
-					/>
-			</div>
-
-			<div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-4 pt-4">
-				<div>
-					{/* <label htmlFor="minAmount" className="block text-sm font-medium text-gray-700  mb-1">
-						Min Amount
-					</label> */}
-					<input
-						type="number"
-						placeholder="Min Amount"
-						value={minAmount}
-						onChange={(e) => setMinAmount(e.target.value)}
-						className="w-full px-4 py-2 h-12 rounded-lg border border-gray-300 bg-white text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-base"
-						/>
-				</div>
-				
-				<div>
-					{/* <label htmlFor="maxAmount" className="block text-sm font-medium text-gray-700  mb-1">
-						Max Amount
-					</label> */}
-					<input
-						type="number"
-						placeholder="Max Amount"
-						value={maxAmount}
-						onChange={(e) => setMaxAmount(e.target.value)}
-						className="w-full px-4 py-2 h-12 rounded-lg border border-gray-300 bg-white text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-base"
-						/>
-				</div>
-			</div>
-
+			{/* Basic filter */}
 			{/* filter buttons */}
 			<div className="flex flex-wrap justify-center gap-2 mb-4 mt-8">
 				<button
@@ -107,6 +74,66 @@ function TransactionFilter({
 					Expense
 				</button>
 			</div>
+
+			{/* Step 2: Conditionally render advanced filters */}
+			{showAdvanced && (
+				<>
+					{/* search field */}
+					<div className="relative">
+						<Search size={18} className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+						<input
+							type="text"
+							placeholder="Search transactions..."
+							value={searchText}
+							onChange={(e) => setSearchText(e.target.value)}
+							className="pl-10 w-full px-4 py-2 h-12 rounded-lg border border-gray-300 bg-white text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-base"
+						/>
+					</div>
+
+					<div className="grid grid-cols-1 sm:grid-cols-2 gap-4 pt-4">
+						<div>
+							{/* <label htmlFor="minAmount" className="block text-sm font-medium text-gray-700  mb-1">
+						Min Amount
+					</label> */}
+							<input
+								type="number"
+								placeholder="Min Amount"
+								value={minAmount}
+								onChange={(e) => setMinAmount(e.target.value)}
+								className="w-full px-4 py-2 h-12 rounded-lg border border-gray-300 bg-white text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-base"
+							/>
+						</div>
+
+						<div>
+							{/* <label htmlFor="maxAmount" className="block text-sm font-medium text-gray-700  mb-1">
+						Max Amount
+					</label> */}
+							<input
+								type="number"
+								placeholder="Max Amount"
+								value={maxAmount}
+								onChange={(e) => setMaxAmount(e.target.value)}
+								className="w-full px-4 py-2 h-12 rounded-lg border border-gray-300 bg-white text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-base"
+							/>
+						</div>
+					</div>
+				</>
+			)}
+
+			<div className="flex items-center mt-6 justify-center">
+				{(filterType !== 'all' || searchText || minAmount || maxAmount) && (
+					<button
+						onClick={handleClearFilters}
+						className="text-sm text-blue-600 hover:underline"
+					>
+						Clear Filters
+					</button>
+				)}
+			</div>
+
+
+
+
 		</div>
 	);
 }
